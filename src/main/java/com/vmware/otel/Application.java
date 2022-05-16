ab16ca578640784744d34e966e75476caddfd25f
Sumit Deo <deosu@vmware.com>
2022-05-16 16:18:10 -0700

    Initial Commit

--- src/main/java/com/vmware/otel/Application.java
+++ src/main/java/com/vmware/otel/Application.java
@@ -1,48 +1,55 @@
 package com.vmware.otel;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.time.Duration;
 
-import io.grpc.ManagedChannel;
-import io.grpc.okhttp.OkHttpChannelBuilder;
-import io.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest;
-import io.opentelemetry.proto.collector.metrics.v1.MetricsServiceGrpc;
-import io.opentelemetry.proto.metrics.v1.AggregationTemporality;
-import io.opentelemetry.proto.metrics.v1.Histogram;
-import io.opentelemetry.proto.metrics.v1.HistogramDataPoint;
-import io.opentelemetry.proto.metrics.v1.InstrumentationLibraryMetrics;
-import io.opentelemetry.proto.metrics.v1.Metric;
-import io.opentelemetry.proto.metrics.v1.ResourceMetrics;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.LongHistogram;
+import io.opentelemetry.exporters.otlp.OtlpGrpcMetricExporter;
+import io.opentelemetry.sdk.metrics.InstrumentSelector;
+import io.opentelemetry.sdk.metrics.InstrumentType;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.View;
+import io.opentelemetry.sdk.metrics.export.MetricReader;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
+import io.opentelemetry.sdk.metrics.internal.view.ExponentialHistogramAggregation;
+import io.opentelemetry.sdk.resources.Resource;
+
+import static io.opentelemetry.api.common.AttributeKey.stringKey;
 
 /**
  * @author Sumit Deo (deosu@vmware.com)
  */
 public class Application {
+  private static final Resource RESOURCE =
+      Resource.create(Attributes.of(stringKey("resource_key"), "resource_value"));
   public static void main(String[] args) {
-    ManagedChannel managedChannel = managedChannel();
-    MetricsServiceGrpc.MetricsServiceBlockingStub metricService =
-        MetricsServiceGrpc.newBlockingStub(managedChannel);
-    HistogramDataPoint point = HistogramDataPoint.newBuilder()
-        .addAllExplicitBounds(Arrays.asList(1.0, 2.0, 3.0))
-        .addAllBucketCounts(Arrays.asList(1L, 2L, 3L, 4L))
-        .setTimeUnixNano(1515151515L)
-        .build();
+    OtlpGrpcMetricExporter metricExporter =
+        OtlpGrpcMetricExporter.newBuilder().setEndpoint("http://localhost:4317").build();
 
-    Histogram otelHistogram = Histogram.newBuilder()
-        .setAggregationTemporality(AggregationTemporality.AGGREGATION_TEMPORALITY_DELTA)
-        .addAllDataPoints(Collections.singletonList(point))
-        .build();
+    MetricReader periodicReader =
+        PeriodicMetricReader.builder(metricExporter).setInterval(Duration.ofMillis(1000)).build();
 
-    Metric otelMetric = Metric.newBuilder()
-        .setHistogram(otelHistogram)
-        .setName("test-delta-histogram")
-        .build();
-    ResourceMetrics resourceMetrics = ResourceMetrics.newBuilder().addInstrumentationLibraryMetrics(InstrumentationLibraryMetrics.newBuilder().addMetrics(otelMetric).build()).build();
-    ExportMetricsServiceRequest request = ExportMetricsServiceRequest.newBuilder().addResourceMetrics(resourceMetrics).build();
-    metricService.export(request);
-  }
+    SdkMeterProvider sdkMeterProvider =
+        SdkMeterProvider.builder()
+            .setResource(RESOURCE)
+            .registerMetricReader(periodicReader)
+            .registerView(
+                InstrumentSelector.builder().setType(InstrumentType.HISTOGRAM).build(),
+                View.builder()
+                    .setAggregation(ExponentialHistogramAggregation.create(-1, 5))
+                    .build())
+            .build();
+    LongHistogram longHistogram =
+        sdkMeterProvider
+            .get(Application.class.getName())
+            .histogramBuilder("testHistogram")
+            .setDescription("description")
+            .setUnit("ms")
+            .ofLongs()
+            .build();
 
-  private static ManagedChannel managedChannel() {
-    return OkHttpChannelBuilder.forAddress("localhost", 4317).usePlaintext().build();
+    longHistogram.record(12L, Attributes.builder().put("key", "value").build());
+    longHistogram.record(12L);
+    longHistogram.record(13L);
   }
 }
