29315a77a81c85e725c3a4b8c25187c3a575e059
Sumit Deo <deosu@vmware.com>
2022-05-17 13:23:56 -0700

    Initial Commit

--- src/main/java/com/vmware/otel/Application.java
+++ src/main/java/com/vmware/otel/Application.java
@@ -2,11 +2,13 @@ package com.vmware.otel;
 
 import java.time.Duration;
 
-import io.grpc.ManagedChannel;
-import io.grpc.okhttp.OkHttpChannelBuilder;
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.LongCounter;
 import io.opentelemetry.api.metrics.LongHistogram;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.metrics.MeterProvider;
 import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
+import io.opentelemetry.proto.metrics.v1.AggregationTemporality;
 import io.opentelemetry.sdk.metrics.InstrumentSelector;
 import io.opentelemetry.sdk.metrics.InstrumentType;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
@@ -24,10 +26,36 @@ import static io.opentelemetry.api.common.AttributeKey.stringKey;
 public class Application {
   private static final Resource RESOURCE =
       Resource.create(Attributes.of(stringKey("resource_key"), "resource_value"));
+
   public static void main(String[] args) {
-    ManagedChannel managedChannel = managedChannel();
-    OtlpGrpcMetricExporter metricExporter =
-        OtlpGrpcMetricExporter.builder().setChannel(managedChannel).build();
+    System.setProperty("otel.resource.attributes", "service.name=OtlpExporterExample");
+    MeterProvider meterProvider = initOpenTelemetryMetrics();
+
+    LongHistogram longHistogram =
+        meterProvider
+            .get("io.opentelemetry.example")
+            .histogramBuilder("testHistogram")
+            .setDescription("description")
+            .setUnit("ms")
+            .ofLongs()
+            .build();
+
+    longHistogram.record(10L);
+    longHistogram.record(20L);
+    longHistogram.record(30L);
+    longHistogram.record(40L);
+    longHistogram.record(50L);
+
+    longHistogram.record(10L);
+    longHistogram.record(40L);
+    longHistogram.record(50L);
+
+    longHistogram.record(10L);
+    longHistogram.record(20L);
+  }
+
+  private static MeterProvider initOpenTelemetryMetrics() {
+    OtlpGrpcMetricExporter metricExporter = OtlpGrpcMetricExporter.getDefault();
 
     MetricReader periodicReader =
         PeriodicMetricReader.builder(metricExporter).setInterval(Duration.ofMillis(1000)).build();
@@ -39,24 +67,11 @@ public class Application {
             .registerView(
                 InstrumentSelector.builder().setType(InstrumentType.HISTOGRAM).build(),
                 View.builder()
-                    .setAggregation(ExponentialHistogramAggregation.create(-1, 5))
+                    .setAggregation(ExponentialHistogramAggregation.create(-1, 10))
                     .build())
             .build();
-    LongHistogram longHistogram =
-        sdkMeterProvider
-            .get(Application.class.getName())
-            .histogramBuilder("testHistogram")
-            .setDescription("description")
-            .setUnit("ms")
-            .ofLongs()
-            .build();
 
-    longHistogram.record(12L, Attributes.builder().put("key", "value").build());
-    longHistogram.record(12L);
-    longHistogram.record(13L);
-  }
-
-  private static ManagedChannel managedChannel() {
-    return OkHttpChannelBuilder.forAddress("localhost", 4317).usePlaintext().build();
+    Runtime.getRuntime().addShutdownHook(new Thread(sdkMeterProvider::shutdown));
+    return sdkMeterProvider;
   }
 }
